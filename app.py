da3c5029c0c61a00ee79a871fbbf33494cc5a4e6
Hank <40827070+Shunian-Chen@users.noreply.github.com>
2022-11-13 04:15:28 +0000

    formatted

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
 import gradio as gr
 from transformers import pipeline
 
+
 def analytics_emo(x):
     data = classifier(x)[0]
     return data
 
+
 if __name__ == "__main__":
-    classifier = pipeline("text-classification", model="Shunian/yelp_review_rating_reberta_base", top_k=1)
+    classifier = pipeline(
+        "text-classification", model="Shunian/yelp_review_rating_reberta_base", top_k=1
+    )
 
     with gr.Blocks() as demo:
-        gr.Markdown("<center><h1>Content Sentiment Analysis</h1> A simple Sentiment Analysis Tool</center>")
+        gr.Markdown(
+            "<center><h1>Content Sentiment Analysis</h1> A simple Sentiment Analysis Tool</center>"
+        )
         with gr.Tab("Review Rating"):
             with gr.Row():
                 with gr.Column(scale=1, min_width=600):
-                    analytics_input = gr.Textbox(label="Text Content", lines=4, max_lines=100, placeholder="Analyzing Sentiment...")
+                    analytics_input = gr.Textbox(
+                        label="Text Content",
+                        lines=4,
+                        max_lines=100,
+                        placeholder="Analyzing Sentiment...",
+                    )
                     analytics_button = gr.Button("Analyze")
-                text_output = gr.Textbox(label="Result", lines=10, max_lines=100, placeholder="Sentiment...")
-        analytics_button.click(analytics_emo, api_name="analytics", inputs=analytics_input, outputs=text_output)
+                text_output = gr.Textbox(
+                    label="Result", lines=10, max_lines=100, placeholder="Sentiment..."
+                )
+        analytics_button.click(
+            analytics_emo,
+            api_name="analytics",
+            inputs=analytics_input,
+            outputs=text_output,
+        )
     demo.launch(debug=True, server_name="0.0.0.0")
