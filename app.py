091e6a67644d943993658de5bcc8424708ff2a94
Hank <40827070+Shunian-Chen@users.noreply.github.com>
2022-11-13 03:15:17 +0000

    interface

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
 
     with gr.Blocks() as demo:
         gr.Markdown("<center><h1>Content Sentiment Analysis</h1> A simple Sentiment Analysis Tool</center>")
-        with gr.Tab("Sentiment Analysis"):
+        with gr.Tab("Review Rating"):
             with gr.Row():
                 with gr.Column(scale=1, min_width=600):
                     analytics_input = gr.Textbox(label="Text Content", lines=4, max_lines=100, placeholder="Analyzing Sentiment...")
                     analytics_button = gr.Button("Analyze")
                 text_output = gr.Textbox(label="Result", lines=10, max_lines=100, placeholder="Sentiment...")
         analytics_button.click(analytics_emo, api_name="analytics", inputs=analytics_input, outputs=text_output)
-    demo.launch(debug=True, server_name="0.0.0.0", share = True)
+    demo.launch(debug=True, server_name="0.0.0.0")
